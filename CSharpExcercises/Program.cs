7cbf6af0b54147b26cb5cf4041404b6af1d82fc5
Santiago <sanfctz@gmail.com>
2020-01-10 23:31:46 -0300

    [Init] Stopwatch excercise

---
+++ CSharpExcercises/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpExcercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+
+                Console.WriteLine($"The interval duration was {stopwatch.GetInterval()}");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ CSharpExcercises/Stopwatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpExcercises
+{
+    public class Stopwatch
+    {
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private bool _running;
+
+        public void Start()
+        {
+            //Check if the stop is already running
+            if (_running)
+                throw new InvalidOperationException("Stopwatch is running!");
+
+            //Set the running flag to true and save the start date
+            _startDate = DateTime.Now;
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            //Check if the stopwatch is not running
+            if (!_running)
+                throw new InvalidOperationException("Stopwatch is not running!");
+
+            _endDate = DateTime.Now;
+            _running = false;
+        }
+
+        public TimeSpan GetInterval() => _endDate - _startDate;
+
+    }
+}
